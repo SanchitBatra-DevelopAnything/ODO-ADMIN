5b0d327b684708e3686bee1805884657d954e60a
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2025-10-11 14:55:47 +0530

    Basic delivery route setup

--- src/app/orders/delivery-route-maker/delivery-route-maker.component.ts
+++ src/app/orders/delivery-route-maker/delivery-route-maker.component.ts
@@ -8,30 +8,66 @@ import { UtilityService } from 'src/app/services/utility/utility.service';
   styleUrls: ['./delivery-route-maker.component.scss']
 })
 export class DeliveryRouteMakerComponent {
+  shopsAvailableForDelivery: any[] = [];
+  deliveryRoutes: any[] = [];
+  uniqueShopsSubscription: Subscription = new Subscription();
+  selectedRoute: any = null;
 
-  shopsAvailableForDelivery:any;
-  uniqueShopsSubscription: any;
+  constructor(private utilityService: UtilityService) {}
 
-  constructor(private utilityService : UtilityService) { }
-  
-    ngOnInit()
-    {
-      this.uniqueShopsSubscription = this.utilityService.sendUniqueShopsForDeliveryRoute.subscribe(
-        (shops: any) => {
-          if(shops?.length > 0)
-          {
-            console.log("Shops received : "+shops);
-            this.shopsAvailableForDelivery = shops;
-          }
+  ngOnInit() {
+    this.uniqueShopsSubscription = this.utilityService.sendUniqueShopsForDeliveryRoute.subscribe(
+      (shops: any) => {
+        if(shops?.length > 0) {
+          this.shopsAvailableForDelivery = shops;
         }
-      );
-    }
-
-    ngOnDestroy() {
-      if (this.uniqueShopsSubscription) {
-        this.uniqueShopsSubscription.unsubscribe();
       }
+    );
+  }
+
+  createNewRoute() {
+    const newRoute = {
+      id: Date.now().toString(),
+      driver: '',
+      shops: []
+    };
+    this.deliveryRoutes.push(newRoute);
+    this.selectedRoute = newRoute;
+  }
+
+  addShopToRoute(shop: any, route: any) {
+    const routeShop = {
+      ...shop,
+      priority: route.shops.length + 1
+    };
+    route.shops.push(routeShop);
+  }
+
+  updateShopPriority(route: any, shop: any, newPriority: number) {
+    const shopIndex = route.shops.findIndex((s: any) => s === shop);
+    if (shopIndex !== -1) {
+      route.shops[shopIndex].priority = newPriority;
+      route.shops.sort((a: any, b: any) => a.priority - b.priority);
+    }
+  }
+
+  assignDriver(route: any, driverName: string) {
+    route.driver = driverName;
+  }
+
+  saveRouteToDatabase(route:any)
+  {
+
+  }
+
+  sendRouteViaWhatsApp(route: any) 
+    {
+
     }
 
-    
+  ngOnDestroy(){
+    if (this.uniqueShopsSubscription) {
+      this.uniqueShopsSubscription.unsubscribe();
+    }
+  }
 }
