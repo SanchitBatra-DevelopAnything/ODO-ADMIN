86f3d6c3b91ea10ad69a73482b35cf036d7ecca2
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2025-11-05 22:10:11 +0530

    Delivery partners add/delete flow created

--- src/app/add-delivery-partner-form/add-delivery-partner-form.component.ts
+++ src/app/add-delivery-partner-form/add-delivery-partner-form.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ApiService } from '../services/api/api.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { UtilityService } from '../services/utility/utility.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-delivery-partner-form',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-delivery-partner-form.component.scss']
 })
 export class AddDeliveryPartnerFormComponent {
-
+  ref:DynamicDialogRef | undefined;
+    isLoading:boolean = false;
+    addDeliveryPartnerForm:any;
+  
+    constructor(private apiService:ApiService , private formBuilder:FormBuilder , private toastr:ToastrService , private utilityService:UtilityService,private dialogService:DialogService){}
+  
+    ngOnInit()
+    {
+      this.addDeliveryPartnerForm = this.formBuilder.group({
+        partnerName: ['', Validators.required],
+        contact : ['' , Validators.required],
+      });
+    }
+  
+    onSubmit(formValue : any)  {
+      if (this.addDeliveryPartnerForm.valid) {
+        this.isLoading = true;
+        this.apiService.addDeliveryPartner(this.addDeliveryPartnerForm.value).subscribe((_:any)=>{
+          this.utilityService.deliveryPartnerAdded.next(true);
+          this.isLoading = false;
+          this.toastr.success('Delivery Partner Added Successfully , Please close the form!', 'Notification!' , {
+            timeOut : 4000 ,
+            closeButton : true , 
+            positionClass : 'toast-top-right'
+          });
+          this.addDeliveryPartnerForm.reset();
+        });
+      }
+    }
 }
